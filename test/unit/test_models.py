e04a6155ae97f5d79eab77c15736952ad5237b04
Eric <eric@simutech.com.tw>
2023-05-23 17:31:30 +0800

    Fix test

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -3,7 +3,7 @@ from matty_web.models import User, add_user, init_db
 
 def test_add_user():
     init_db()
-    u, pw = add_user(username="drunkwcodes", email="eric@simutech.com.tw")
+    add_user(username="drunkwcodes", email="eric@simutech.com.tw")
     assert User.get(
         User.username == "drunkwcodes" and User.email == "eric@simutech.com.tw"
     )
