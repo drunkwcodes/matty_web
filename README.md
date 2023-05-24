057eea2a224a536376857646d8b14ceca1638c76
Eric <eric@simutech.com.tw>
2023-05-24 17:51:18 +0800

    Login page OK

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # matty_web
 
 Abaqus Material Library on WEB
+
+### Reference
+
+https://www.campusplastics.com/
+
+https://materials.ulprospector.com/en
+
+
+https://www.matweb.com/
+
+
+### TODO
+
+Matweb 爬蟲，轉檔成 .lib
--- src/matty_web/views.py
+++ src/matty_web/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_admin.contrib.peewee import ModelView
 
 from .models import User, UserInfo
@@ -8,4 +8,9 @@ my_blueprint = Blueprint("my_blueprint", __name__)
 
 @my_blueprint.route("/")
 def index():
-    return '<a href="/admin/">Click me to get to Admin!</a>'
+    return render_template("index.html")
+
+
+@my_blueprint.route("/login")
+def login():
+    return render_template("login.html")
